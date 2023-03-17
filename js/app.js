ef2a6553afb61c0b16561fe365f69d58f8881d45
markb <markbuchholz8@gmail.com>
2023-03-16 20:59:34 -0600

    speeling checks

--- js/app.js
+++ js/app.js
@@ -1,28 +1,28 @@
 let userage=18;
 let is_subscribed= true;
 
-if(is_subscribed === ture && userage >=18){
+if(is_subscribed === true && userage >=18){
     console.log("The user is 18, and subscribed. Welcome!")
 } else {
     console.log("The user is not 18, or subscribed")
-}
-
-if(is_subscribed === false && userage >=18){
+}if(is_subscribed === false && userage >=18){
     console.log("The user is 18, but not subscribed. Sorry!")
 } else {
     console.log("Please Subscribe")
-}
-
-if(is_subscribed === false && userage <=18){
+}if(is_subscribed === false && userage <18){
     console.log("The User is not 18, and not subscribed. Sorry!")
 } else {
     console.log("Must be 18 years old, and Please Subscribe")
-}
-
-if(is_subscribed === Ture && userage <=18){
+}if(is_subscribed === True && userage <18){
     console.log("The user is not 18, however subscribed")
 } else {
     console.log("Must be 18 years old")
 }
+
+
+
+
+
+
 
 
