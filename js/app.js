b0c6f2469a2c88e31779478966f991099949ac1d
markb <markbuchholz8@gmail.com>
2023-03-18 09:43:17 -0600

    else if

--- js/app.js
+++ js/app.js
@@ -3,20 +3,12 @@ let is_subscribed= true;
 
 if(is_subscribed === true && userage >=18){
     console.log("The user is 18, and subscribed. Welcome!")
-} else {
+} else if (is_subscribed === false && userage >=18){
+    console.log("The user is 18, and not subscribed. Sorry!")
+} else if (is_subscribed === false && userage <18){
     console.log("The user is not 18, or subscribed")
-}if(is_subscribed === false && userage >=18){
-    console.log("The user is 18, but not subscribed. Sorry!")
-} else {
-    console.log("Please Subscribe")
-}if(is_subscribed === false && userage <18){
-    console.log("The User is not 18, and not subscribed. Sorry!")
-} else {
-    console.log("Must be 18 years old, and Please Subscribe")
-}if(is_subscribed === True && userage <18){
-    console.log("The user is not 18, however subscribed")
-} else {
-    console.log("Must be 18 years old")
+} else if (is_subscribed === true && userage <18){
+    console.log("user is not 18")
 }
 
 
